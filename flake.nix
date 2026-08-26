dede8ad041a5f2ea0f608bd7aaea8860056c0cb2
egigoka <egigoka@gmail.com>
2026-08-26 14:25:27 +0500

    Prepare Desktop v0.4.4 release

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         {
           default = pkgs.python3.pkgs.buildPythonApplication {
             pname = "pomodorough-linux";
-            version = "0.4.3";
+            version = "0.4.4";
             pyproject = true;
             src = pkgs.lib.fileset.toSource {
               root = ./.;
--- src/pomodorough/__init__.py
+++ src/pomodorough/__init__.py
@@ -1,3 +1,3 @@
 """Pomodorough desktop client."""
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
