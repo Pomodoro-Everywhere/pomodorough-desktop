558715c8cfc5669137a8dc6d16ccf03cdd615f8e
egigoka <egigoka@gmail.com>
2026-09-08 18:44:11 +0500

    release desktop 0.20.0

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         {
           default = pkgs.python3.pkgs.buildPythonApplication {
             pname = "pomodorough-linux";
-            version = "0.19.0";
+            version = "0.20.0";
             pyproject = true;
             src = pkgs.lib.fileset.toSource {
               root = ./.;
--- src/pomodorough/shared_core.py
+++ src/pomodorough/shared_core.py
@@ -13,8 +13,8 @@ from typing import Any, Final, Protocol, Self
 
 from wasmtime import Engine, Func, Instance, Memory, Module, Store, WasmtimeError
 
-CORE_COMMIT: Final = "e90929f6877c1a07e13964d00b942f09eeb5dfa8"
-CORE_SHA256: Final = "658c3c60a90f7ee0b2131b4af8fa10b60c3f79417789a41bea5ea6712ea5a619"
+CORE_COMMIT: Final = "9a77f1d3f90cd1f1a33ddb77d447a6eca05ed43f"
+CORE_SHA256: Final = "58a906927bae3b184684fffa7825cf46cd4bf71326533128372881ed0275b079"
 WASM_RESOURCE: Final = "pomodorough_core.wasm"
 
 _MAX_OPERATION_BYTES: Final = 256
