573f577ce92eb4c195c3529de9dad20eac7326eb
egigoka <egigoka@gmail.com>
2026-09-10 00:11:04 +0500

    release desktop 0.22.1

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         {
           default = pkgs.python3.pkgs.buildPythonApplication {
             pname = "pomodorough-linux";
-            version = "0.22.0";
+            version = "0.22.1";
             pyproject = true;
             src = pkgs.lib.fileset.toSource {
               root = ./.;
