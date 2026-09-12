7b19c85848adc99032f79be19ec0eda915c27da8
egigoka <egigoka@gmail.com>
2026-09-12 11:25:04 +0500

    release desktop 0.31.0

--- src/pomodorough/shared_core.py
+++ src/pomodorough/shared_core.py
@@ -13,8 +13,8 @@ from typing import Any, Final, Protocol, Self
 
 from wasmtime import Engine, Func, Instance, Memory, Module, Store, WasmtimeError
 
-CORE_COMMIT: Final = "7cdc404cb5d0ae7aed0c675dfc9e1c0faf8f39af"
-CORE_SHA256: Final = "3fec2bf5ea6fc0c2a8ddec862fbaed0cb2fabe14e466ee4dfa3fb80b99d4da49"
+CORE_COMMIT: Final = "2af512330656b956839198f0c019b0031b2c1a8f"
+CORE_SHA256: Final = "7b1436dc24e7fedb52f52bd193be78a775f062bdfc4fcef25bb41caf5325e379"
 WASM_RESOURCE: Final = "pomodorough_core.wasm"
 
 _MAX_OPERATION_BYTES: Final = 256
